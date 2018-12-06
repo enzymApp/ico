e37c26b8c3ca8792279e35e0a6077cacf9096c89
unknown <adenys.ad@gmail.com>
2018-12-06 20:38:12 +0100

    Debut de la réorganisation des styles, import du plugin fourseven pour la compilation scss, import de l'autoprefixer qui lui est associé

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Routes from './Routes'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './main.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+
+//Import des styles
+import './styles/main.scss'
 
 export default () => {
   return (
