bac98ae96cccb5e6330bb47d13ea98d74dd54638
unknown <adenys.ad@gmail.com>
2018-12-11 21:04:22 +0100

    retravail du bloc promo

--- imports/ui/Routes.js
+++ imports/ui/Routes.js
@@ -3,12 +3,19 @@ import {Route,
         Switch,
         Redirect}            from 'react-router'
 import {BrowserRouter}       from 'react-router-dom'
+import {Helmet}              from 'react-helmet'
 
 import Home               from '/imports/ui/pages/Home'
 
 export default () => (
   <BrowserRouter>
     <div>
+      <Helmet>
+      <title>Enzym - ICO Website</title>
+      <meta name="keywords" content="Enzym ICO Website" />
+      <meta name="description"content="Enzym ICO Website"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>
+      </Helmet>
       <Switch>
         <Route name="home" path="/" component={Home}/>
       </Switch>
--- imports/ui/layouts/Main.js
+++ imports/ui/layouts/Main.js
@@ -1,64 +1,74 @@
 import React from 'react'
-import {Row, Col} from 'reactstrap'
+import {Container, Row, Col} from 'reactstrap'
 import T              from '../components/Translator'
 
 export default ({children, isUserPage}) => {
   return (
     <div className="background_img_top">
-      <Row id="main">
-        <Col xs="12" sm="6" md="8">
-          <div id="visual">
-            <h1>
-              <T>Ico.intro.big</T><br/><T>Ico.intro.big2</T>
-            </h1>
-            <h3><T>Ico.intro.small</T></h3>
-            <img src="/images/application-enzym.png" className="mockup"/>
-          </div>
-          <Row className="cta">
-            <Col xs="12" sm="12" md="4">
-              <div class="whitepaper">
-                <div class="outline">
-                  WHITEPAPER
-                  <div class="sub-lang">
-                    <a href="">FR</a><br/>
-                    <a href="">EN</a><br/>
-                    <a href="">CH</a><br/>
-                    <a href="">RUS</a><br/>
-                    <a href="">AR</a><br/>
+      <Container>
+        <Row id="main">
+          <Col xs="12" sm="6" md="8">
+            <div id="visual">
+              <h1>
+                <T>Ico.intro.big</T><br/><T>Ico.intro.big2</T>
+              </h1>
+              <h3><T>Ico.intro.small</T></h3>
+              <img src="/images/application-enzym.png" className="mockup"/>
+            </div>
+            <Row className="cta">
+              <Col xs="12" sm="12" md="4">
+                <div className="whitepaper">
+                  <div className="outline">
+                    WHITEPAPER
+                    <div className="sub-lang">
+                      <a href="">FR</a><br/>
+                      <a href="">EN</a><br/>
+                      <a href="">CH</a><br/>
+                      <a href="">RUS</a><br/>
+                      <a href="">AR</a><br/>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Col>
-            <Col xs="12" sm="12" md="4">
-              <a href="https://itunes.apple.com/app/id1437880226" class="app" target="_blank">GO TO APP STORE</a>
-            </Col>
-            <Col xs="12" sm="12" md="4">
-              <a href="https://play.google.com/store/apps/details?id=com.enzym_proto" class="app" target="_blank">GO TO PLAY STORE</a>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs="12" sm="12" md="4">
-          <div id="block">
-            <div id="top_bloc">
-              <div className="title">
-                <h2><T>Ico.token</T></h2>
-                <h3><T>Ico.start</T><br/><T>Ico.date</T></h3>
-              </div>
-              <p><T>Ico.para1</T></p>
-              <hr />
-              <div class="goal">
-                <h3><T>Ico.goal</T></h3>
-                <div class="center">
-                  <img src="/images/picto/eth.png" className="eth"/>
+              </Col>
+              <Col xs="12" sm="12" md="4">
+                <a href="https://itunes.apple.com/app/id1437880226" className="app" target="_blank">GO TO APP STORE</a>
+              </Col>
+              <Col xs="12" sm="12" md="4">
+                <a href="https://play.google.com/store/apps/details?id=com.enzym_proto" className="app" target="_blank">GO TO PLAY STORE</a>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs="12" sm="12" md="4">
+            <div className="bloc-promo">
+              <div id="top_bloc">
+                <div className="title">
+                  <h2><T>Ico.token</T></h2>
                 </div>
-                <h3>20 000 000 ETH</h3>
+
+                <div className="bloc-promo__content outline text-center">
+                  <p className=""><T>Ico.para1</T></p>
+                  <h3 className="bloc-promo__title"><T>Ico.para2</T></h3>
+                  <p className="bloc-promo__title bloc-promo__title--promo"><T>Ico.para3</T></p>
+                  <a href="mailto:ico@enzym.io" className="btn" target="_blank"><T>Ico.link</T></a>
+                </div>
+
+                <div className="bloc-promo__content outline text-center">
+                  <p className=""><T>Ico.para4</T></p>
+                  <h3 className="bloc-promo__title"><T>Ico.para5</T></h3>
+                  <p className="bloc-promo__title bloc-promo__title--promo"><T>Ico.para6</T></p>
+                </div>
+
+                <div className="goal text-center">
+                  <p><T>Ico.para7</T></p>
+                  <img src="/images/picto/eth.png" className="eth"/>
+                  <h3>25 000 ETH</h3>
+                </div>
+
               </div>
-              <hr />
-              <p><T>Ico.para2</T></p>
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
