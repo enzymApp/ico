1c85366e339f137ce5bb08a15c94afb10ea6aad6
unknown <adenys.ad@gmail.com>
2018-12-17 21:08:57 +0100

    Ajout du bouton Chat

--- imports/ui/layouts/Chat.js
+++ imports/ui/layouts/Chat.js
@@ -5,9 +5,11 @@ export default class Chat extends React.Component {
 
   render() {
     return (
-      <div class="chat">
-
-      </div>
+      <a href="#" target="_blank" class="chat_ico" title="Chat Telegram">
+        <div class="chat_ico__content">
+        <img src="/images/picto/logo-telegram.svg" title="Chat Telegram" alt="Chat telegram"/>
+        </div>
+      </a>
     )
   }
 }
